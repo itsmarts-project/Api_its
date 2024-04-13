a4a8ab09a93a96cb99d7d20fdf7923e26d45c684
oscar <oscarmancilla227@gmail.com>
2024-04-12 21:42:59 -0600

    amen

--- database/configdb.ts
+++ database/configdb.ts
@@ -2,6 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config();
 
+
 const databaseConnection = new Sequelize('itsmarts', 'doadmin', process.env.PASSWORD_DB, {
     host: process.env.HOST_DB,
     dialect: 'postgres',
--- helpers/subirFoto.ts
+++ helpers/subirFoto.ts
@@ -20,19 +20,12 @@ export const subirArchivo = async(files: UploadedFile, extensionesPermitidas = [
         }
     
         const finalFileName: string =  uuidv4() + '.' + extension;
-        const uploadPath = path.join(__dirname, '../../uploads' ,carpeta,finalFileName);
-        const uploadPath2 = path.join(__dirname,'../uploads', carpeta, finalFileName );
-        
-    
+        const uploadPath = path.resolve(__dirname, '../../uploads' ,carpeta,finalFileName);
       
-        // Use the mv() method to place the file somewhere on your server
+
         
         archivo.mv(uploadPath, function(err: Error) {
           if (err) {return reject}
-        });
-        archivo.mv(uploadPath2, function(err: Error){
-            if(err){return reject}
-          
         });
 
       
