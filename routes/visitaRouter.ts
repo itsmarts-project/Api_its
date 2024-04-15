759bf4e446cbbcaea1c7e8bc22ebb74e09a29c6d
oscar <oscarmancilla227@gmail.com>
2024-04-14 20:36:33 -0600

    se agregaron validaciones a visita

--- routes/visitaRouter.ts
+++ routes/visitaRouter.ts
@@ -3,12 +3,28 @@ import { agregarEstatusVisita, confirmarVisita, getFotoSolicitante, getVisitasPe
 import { validarArchivo } from "../middlewares/validarArchivo";
 import validarJWT from "../middlewares/validarToken";
 import validarCampos from "../middlewares/ValidarErrores";
+import { body } from "express-validator";
 
 const visitaRouter = Router();
 
-visitaRouter.post("/visitasPendientes",getVisitasPendientes);
-visitaRouter.put("/actualizarEstatus",agregarEstatusVisita);
-visitaRouter.put("/confirmarVisita", confirmarVisita);
-visitaRouter.post("/fotoSolicitante", getFotoSolicitante);
+visitaRouter.post("/visitasPendientes",[
+    validarJWT,
+    body('id').notEmpty()
+],getVisitasPendientes);
+
+visitaRouter.put("/actualizarEstatus",[
+    validarJWT,
+    body('id').notEmpty
+],agregarEstatusVisita);
+
+visitaRouter.put("/confirmarVisita", [
+    validarJWT,
+    body('id').notEmpty()
+],confirmarVisita);
+
+visitaRouter.post("/fotoSolicitante",[
+    validarJWT,
+    body('id').notEmpty()
+],getFotoSolicitante);
 
 export default visitaRouter;
